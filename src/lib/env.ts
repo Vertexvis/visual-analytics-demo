f68cf19f3703c22eb8b262fdca1cb64eeb3acfa6
Rocky Warren <1085683+therockstorm@users.noreply.github.com>
2021-05-27 14:00:05 -0600

    Update deps, default stream key

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -8,7 +8,7 @@ export interface StreamCredentials {
 // Super Car
 export const DefaultCredentials: StreamCredentials = {
   clientId: "08F675C4AACE8C0214362DB5EFD4FACAFA556D463ECA00877CB225157EF58BFA",
-  streamKey: "RA00uJtbA41VR9NmZvHnXSW4H8viv9AI5Vbx",
+  streamKey: "2M8D6GttDum511RmQBsb3iw6qjj9HTFTC_GJ",
 };
 
 export const Env =
