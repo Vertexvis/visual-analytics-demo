ac68a4bdefc3b265f54de6bf8022ee860238a6eb
Madison Ehlers <49169871+MadisonEhlers-Vertex@users.noreply.github.com>
2024-03-05 14:22:18 -0600

    [PLAT-4007] Updating Demo Stream Key (#23)

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -8,7 +8,7 @@ export interface StreamCredentials {
 // Super Car
 export const DefaultCredentials: StreamCredentials = {
   clientId: "08F675C4AACE8C0214362DB5EFD4FACAFA556D463ECA00877CB225157EF58BFA",
-  streamKey: "xhh2xijQy-XO9aHtS-P5Rl51vGmadZUCrfPj",
+  streamKey: "xwa3EgD3xfeETw164U9XmxopKZ0c8n1gt93j",
 };
 
 export const Env =
@@ -16,7 +16,7 @@ export const Env =
 
 export const SampleDataPaths: { [k: string]: string } = {
   [DefaultCredentials.streamKey]: "/super-car-data.zip",
-  "hfmI8VBsIiMmO7THqdZdWO5Fhjqp_VDaee9e": "/vertex-valve-data.zip",
+  "AH7v0jg5aN5_thkhU-XTzB_29aqW89EjyOH8": "/vertex-valve-data.zip",
 };
 
 export function head<T>(items?: T | T[]): T | undefined {
